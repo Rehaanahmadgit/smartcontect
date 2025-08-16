a94aada086386dfeef154a9510f03a9e451357a8
Rehaanahmadgit <rehaansufyaan1@gmail.com>
2025-08-15 23:43:35 -0500

    start

---
+++ src/test/java/com/example/cry/CryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
